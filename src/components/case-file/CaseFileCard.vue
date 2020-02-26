3172673eb6bb0a7640c3172a54487fb232157005
Charles Kayser <charles.kayser@uspsector.com>
2020-02-26 00:21:51 -0500

    Setup the Case File Card which points to the report view

--- src/components/case-file/CaseFileCard.vue
+++ src/components/case-file/CaseFileCard.vue
@@ -1,8 +1,14 @@
 <template>
   <v-card>
-    <v-btn color="primary">
-      Read Case File
+    <v-btn
+      :to="caseFileReportPath"
+    >
+      <div class="primary-content-button-text">
+        Read Case File
+      </div>
     </v-btn>
+    <!-- Create a service for loading a contrived single case file from db.json;
+    Grab the data; do a router link and pass that data to the CaseFileReportCard as a prop -->
   </v-card>
 </template>
 
@@ -14,6 +20,6 @@ import Component from 'vue-class-component'
   name: 'CaseFileCard'
 })
 export default class CaseFileCard extends Vue {
-
+  private caseFileReportPath: string = '/case-file-report'
 }
 </script>

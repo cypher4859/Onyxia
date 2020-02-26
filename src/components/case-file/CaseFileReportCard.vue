42a94d5cabd4a93fa0fc9e9fbc61058cc5bf57bf
Charles Kayser <charles.kayser@uspsector.com>
2020-02-26 00:21:04 -0500

    Built the Case File Report Card - contains the primary content to be displayed

---
+++ src/components/case-file/CaseFileReportCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card>
+    <v-btn @click="goBack">
+      <div class="primary-content-button-text">
+        BACK
+      </div>
+    </v-btn>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({
+  name: 'CaseFileCard'
+})
+export default class CaseFileCard extends Vue {
+  private goBack () {
+    this.$router.back()
+  }
+}
+</script>

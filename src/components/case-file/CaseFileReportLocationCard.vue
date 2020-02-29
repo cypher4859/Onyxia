a7dcf7d1b4cb86970d4f9fe62ebf30b7f89f3768
Charles Kayser <charles.kayser@uspsector.com>
2020-02-29 03:16:45 -0500

    unused import

--- src/components/case-file/CaseFileReportLocationCard.vue
+++ src/components/case-file/CaseFileReportLocationCard.vue
@@ -9,7 +9,6 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 import ILocation from '@/types/ILocation'
 import CaseFileReportBaseDataCard from './CaseFileReportBaseDataCard.vue'
-import { Prop } from 'vue-property-decorator'
 
 @Component({
   name: 'CaseFileReportLocationCard'
--- src/components/case-file/CaseFileReportReferences.vue
+++ src/components/case-file/CaseFileReportReferences.vue
@@ -9,7 +9,6 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 import IReferences from '@/types/IReferences'
 import CaseFileReportBaseDataCard from '@/components/case-file/CaseFileReportBaseDataCard.vue'
-import { Prop } from 'vue-property-decorator'
 
 @Component({
   name: 'CaseFileReportReferencesCard'

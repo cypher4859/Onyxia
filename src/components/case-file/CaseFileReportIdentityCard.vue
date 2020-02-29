2b04db0bae820f2f031b7306a3ba05bfca477a5f
Charles Kayser <charles.kayser@uspsector.com>
2020-02-29 01:55:29 -0500

    Created and made lots of progress on Case File Report Identity Card. Lots of cool stuff

---
+++ src/components/case-file/CaseFileReportIdentityCard.vue
@@ -0,0 +1,157 @@
+<template>
+  <div>
+    <v-row>
+      <v-col
+        cols="8"
+        md="3"
+        offset="1"
+      >
+        <v-text-field
+          v-model="reportModel['full-name']"
+          label="Full Name"
+          color="success"
+          outlined
+          shaped
+          success
+          :readonly="isEditable"
+        />
+      </v-col>
+      <v-col
+        cols="8"
+        md="3"
+        offset="2"
+      >
+        <v-text-field
+          v-model="reportModel['social-security-number']"
+          label="Social Security Number"
+          color="success"
+          outlined
+          shaped
+          success
+          :readonly="isEditable"
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        cols="6"
+        md="2"
+        offset="1"
+      >
+        <v-text-field
+          v-model="reportModel['month']"
+          label="Month"
+          color="success"
+          outlined
+          shaped
+          success
+          :readonly="isEditable"
+        />
+      </v-col>
+      <v-col
+        cols="6"
+        md="2"
+        offset="1"
+      >
+        <v-text-field
+          v-model="reportModel['day']"
+          label="Day"
+          color="success"
+          outlined
+          shaped
+          success
+          :readonly="isEditable"
+        />
+      </v-col>
+      <v-col
+        cols="6"
+        md="2"
+        offset="1"
+      >
+        <v-text-field
+          v-model="reportModel['year']"
+          label="Year"
+          color="success"
+          outlined
+          shaped
+          success
+          :readonly="isEditable"
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        cols="6"
+        md="2"
+        offset="1"
+      >
+        <v-text-field
+          v-model="reportModel['first']"
+          label="First Name"
+          color="success"
+          outlined
+          shaped
+          success
+          :readonly="isEditable"
+        />
+      </v-col>
+      <v-col
+        cols="6"
+        md="2"
+      >
+        <v-text-field
+          v-model="reportModel['middle']"
+          label="Middle Name"
+          color="success"
+          outlined
+          shaped
+          success
+          :readonly="isEditable"
+        />
+      </v-col>
+      <v-col
+        cols="6"
+        md="2"
+      >
+        <v-text-field
+          v-model="reportModel['last']"
+          label="Last Name"
+          color="success"
+          outlined
+          shaped
+          success
+          :readonly="isEditable"
+        />
+      </v-col>
+      <v-col
+        cols="6"
+        md="2"
+      >
+        <v-text-field
+          v-model="reportModel['additional']"
+          label="Additional Names"
+          color="success"
+          outlined
+          shaped
+          success
+          :readonly="isEditable"
+        />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import IIdentity from '@/types/IIdentity'
+import CaseFileReportBaseDataCard from './CaseFileReportBaseDataCard.vue'
+import { Prop } from 'vue-property-decorator'
+
+@Component({
+  name: 'CaseFileReportIdentityCard'
+})
+export default class CaseFileReportIdentityCard extends CaseFileReportBaseDataCard {
+  private isEditable: boolean = false
+}
+</script>

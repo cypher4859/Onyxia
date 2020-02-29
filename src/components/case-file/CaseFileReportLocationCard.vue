386b2cb6ce202dae99e1ddeb37402127f9545998
Charles Kayser <charles.kayser@uspsector.com>
2020-02-29 01:55:42 -0500

    Created the Location and Reference cards for Reporting

---
+++ src/components/case-file/CaseFileReportLocationCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ reportModel }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import ILocation from '@/types/ILocation'
+import CaseFileReportBaseDataCard from './CaseFileReportBaseDataCard.vue'
+import { Prop } from 'vue-property-decorator'
+
+@Component({
+  name: 'CaseFileReportLocationCard'
+})
+export default class CaseFileReportLocationCard extends CaseFileReportBaseDataCard {
+}
+</script>
---
+++ src/components/case-file/CaseFileReportReferences.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ reportModel }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import IReferences from '@/types/IReferences'
+import CaseFileReportBaseDataCard from '@/components/case-file/CaseFileReportBaseDataCard.vue'
+import { Prop } from 'vue-property-decorator'
+
+@Component({
+  name: 'CaseFileReportReferencesCard'
+})
+export default class CaseFileReportReferencesCard extends CaseFileReportBaseDataCard {
+}
+</script>

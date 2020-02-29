3c1716a17786346cc5688503b6163837515b916a
Charles Kayser <charles.kayser@uspsector.com>
2020-02-29 01:54:46 -0500

    Created the base card for all case file report pieces, like identity, location, and references

---
+++ src/components/case-file/CaseFileReportBaseDataCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import ILocation from '@/types/ILocation'
+import IPersonOfInterest from '@/types/IPersonOfInterest'
+import { Prop } from 'vue-property-decorator'
+
+@Component({
+  name: 'CaseFileReportBaseDataCard'
+})
+export default class CaseFileReportBaseDataCard extends Vue {
+  @Prop(String) readonly reportModel! : IPersonOfInterest
+}
+</script>

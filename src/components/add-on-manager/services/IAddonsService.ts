f4a76dd61f31617228fe8e1716849d31eff89a69
cypher4859 <cypher4859.githubcontact@opaquecipher.33mail.com>
2020-03-21 17:14:51 -0400

    Designed the interface for Addon service, object, and store

--- src/components/add-on-manager/services/IAddonsService.ts
+++ src/components/add-on-manager/services/IAddonsService.ts
@@ -1,7 +1,10 @@
-import IService from '../../../services/interfaces/IService'
+import IService from '@/services/interfaces/IService'
 import IMenuItem from '@/types/IMenuItem'
+import IAddon from '../types/IAddon'
 
 export default interface IAddonsService extends IService {
-    getEnabledAddons(): IMenuItem[]
-    enableAddon(addOnModel: IMenuItem): void
+    getEnabledAddons(): IMenuItem[],
+    enableAddon(addOnModel: IMenuItem): void,
+    getRegisteredAddons(): IAddon[]
+
 }

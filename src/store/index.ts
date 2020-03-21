99c9b081d537bcd52f38f45b7a73f1e4d8dd1c4d
cypher4859 <cypher4859.githubcontact@opaquecipher.33mail.com>
2020-03-21 17:09:41 -0400

    Registered all the stores in the primary root store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import AddonStore from '@/components/add-on-manager/state-management/AddonStore'
+import CameraMonitorStore from '@/components/camera-monitor/state-management/CameraMonitorStore'
+import CaseFileStore from '@/components/case-file/state-management/CaseFileStore'
+import NetMonitorStore from '@/components/network-monitor/state-management/NetMonitorStore'
+import HomeDashboardStore from '@/components/home-dashboard/state-management/HomeDashboardStore'
 
 Vue.use(Vuex)
 

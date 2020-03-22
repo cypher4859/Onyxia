ba3f24adcd2e24179190023db07033abd6dfcd9e
cypher4859 <cypher4859.githubcontact@opaquecipher.33mail.com>
2020-03-22 03:06:22 -0400

    Removed dead imports in the Store because the modules are getting injected in

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,6 @@
+import 'reflect-metadata'
 import Vue from 'vue'
 import Vuex from 'vuex'
-import AddonStore from '@/components/add-on-manager/state-management/AddonStore'
-import CameraMonitorStore from '@/components/camera-monitor/state-management/CameraMonitorStore'
-import CaseFileStore from '@/components/case-file/state-management/CaseFileStore'
-import NetMonitorStore from '@/components/network-monitor/state-management/NetMonitorStore'
-import HomeDashboardStore from '@/components/home-dashboard/state-management/HomeDashboardStore'
 
 Vue.use(Vuex)
 

add006a4f697f808dec9ed5cc0c6ec3de2e74f94
cypher4859 <cypher4859.githubcontact@opaquecipher.33mail.com>
2020-04-18 23:39:59 -0400

    Created a generic snackbar alert for system messages
    Will likely change this up a bit later to suit style

---
+++ src/components/utility/SystemSnackbarAlert.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-snackbar
+      v-model="showIsEditableSnackbar"
+      top
+      left
+      :timeout="1800"
+    >
+      <span color="success">
+        {{ snackBarMessage }}
+      </span>
+    </v-snackbar>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+
+@Component({
+  name: 'SystemSnackbarAlert'
+})
+export default class SystemSnackbarAlert extends Vue {
+  @Prop(String) readonly snackBarMessage!: string
+  @Prop(Boolean) readonly showIsEditableSnackbar!: boolean
+}
+</script>

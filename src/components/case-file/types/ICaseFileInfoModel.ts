2ced3b1e00c538ce0c2723bd08c32cdaccd477ee
cypher4859 <cypher4859.githubcontact@opaquecipher.33mail.com>
2020-05-03 15:28:01 -0400

    Created the model interface for casefile info

---
+++ src/components/case-file/types/ICaseFileInfoModel.ts
@@ -0,0 +1,10 @@
+export default interface ICaseFileInfoModel {
+  name: string
+  age: string
+  height: string
+  weight: string
+  lastLocation: string
+  phone: string
+  email: string
+  relationship: string
+}

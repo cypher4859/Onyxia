d9b5b3bbf40fb3761c83550d21a1b895b67b88a6
cypher4859 <cypher4859.githubcontact@opaquecipher.33mail.com>
2020-05-03 15:33:38 -0400

    Created and implemented majority of features in the case file info bar - might be able to make it look prettier but for first steps it looks pretty good TG-96

---
+++ src/components/case-file/render/CaseFileInfoBar.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-toolbar prominent>
+          <v-col>
+            <v-img
+              src="@/assets/empty-person.png"
+              max-height="160"
+              max-width="93"
+            />
+          </v-col>
+          <v-col>
+            <v-toolbar-title>James R. Watts</v-toolbar-title>
+          </v-col>
+          <v-spacer />
+          <v-col>
+            <div>{{ model.age }}</div>
+            <div>{{ model.height }}</div>
+            <div>{{ model.weight }}</div>
+          </v-col>
+          <v-col>
+            <div>{{ model.lastLocation }}</div>
+            <div>{{ model.phone }}</div>
+            <div>{{ model.email }}</div>
+            <div>{{ model.relationship }}</div>
+          </v-col>
+          <v-spacer />
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+          <v-btn
+            icon
+            color="success"
+            :to="{ path: caseFileReportPath, params: { targetPerson: model }}"
+          >
+            <v-icon>mdi-export</v-icon>
+          </v-btn>
+        </v-toolbar>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import ICaseFileInfoModel from '../types/ICaseFileInfoModel'
+import ICaseFileInfoService from '../services/ICaseFileInfoService'
+import { inject } from 'inversify-props'
+import TYPES from '@/InjectableTypes/types'
+import { Prop } from 'vue-property-decorator'
+
+@Component({
+  name: 'CaseFileInfoBar'
+})
+export default class CaseFileInfoBar extends Vue {
+  @Prop(Object) readonly model!: ICaseFileInfoModel
+  private caseFileReportPath: string = '/case-file-report'
+
+  mounted () {
+    this.setDefaults()
+  }
+
+  public setDefaults () {
+    this.model.age = '44'
+    this.model.name = 'James R. Watts'
+    this.model.height = '6\'4'
+    this.model.weight = '180lbs'
+    this.model.lastLocation = 'Salt Lake City, UT'
+    this.model.phone = '304-442-4328'
+    this.model.email = 'someAlias@someemail.com'
+    this.model.relationship = 'Friend'
+  }
+}
+</script>

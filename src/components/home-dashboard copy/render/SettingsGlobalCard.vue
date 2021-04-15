db8abc94500a37f9ee3cb3ee78abb666f218179e
cypher4859 <cypher4859.githubcontact@opaquecipher.33mail.com>
2021-04-15 11:31:17 -0400

    Added the view and card components of the global settings

---
+++ src/components/home-dashboard copy/render/SettingsGlobalCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <addon-manager />
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import AddOnsView from '@/components/add-on-manager/AddOnsView.vue'
+
+@Component({
+  name: 'SettingsGlobalCard',
+  components: {
+    'addon-manager': AddOnsView
+  }
+})
+export default class SettingsGlobalCard extends Vue {
+
+}
+</script>

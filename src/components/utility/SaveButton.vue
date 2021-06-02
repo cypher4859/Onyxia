9dd4d89ea9ae17b47e685db209bafbe901e59367
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2021-06-02 19:51:11 -0400

    Added the SaveButton

---
+++ src/components/utility/SaveButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-btn @click="$emit('save')">
+      <div class="primary-content-button-text">
+        Save
+      </div>
+    </v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({
+  name: 'SaveButton'
+})
+export default class SaveButton extends Vue {
+
+}
+</script>

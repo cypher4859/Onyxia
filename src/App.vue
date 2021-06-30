a70601aed11d5475f1c273cbc11b74b7f3f299ba
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2021-06-30 18:37:57 -0400

    Started dealing with the user authentication

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
-  <amplify-authenticator>
-    <v-app>
-      <v-main
-        class="fill-height"
-        fluid
-      >
-        <nav-drawer />
-      </v-main>
-    </v-app>
-    <amplify-sign-out />
-  </amplify-authenticator>
+  <v-app>
+    <div
+      v-if="!signedIn"
+    >
+      You are signed out
+    </div>
+    <amplify-auth-container>
+      <amplify-authenticator>
+        <div v-if="signedIn">
+          <v-main
+            class="fill-height"
+            fluid
+          >
+            <nav-drawer />
+          </v-main>
+        </div>
+        <amplify-sign-out />
+      </amplify-authenticator>
+    </amplify-auth-container>
+  </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 import NavDrawer from '@/components/NavDrawer.vue'
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
 
 @Component({
   name: 'app',
@@ -25,6 +35,24 @@ import NavDrawer from '@/components/NavDrawer.vue'
 })
 
 export default class App extends Vue {
-  //
+  private user!: object | undefined
+  private authState!: any
+
+  get signedIn () : boolean {
+    return true
+  }
+
+  private unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
+    this.authState = authState
+    this.user = authData
+  })
+
+  created () {
+    this.unsubscribeAuth()
+  }
+
+  beforeDestroy () {
+    this.unsubscribeAuth()
+  }
 }
 </script>
